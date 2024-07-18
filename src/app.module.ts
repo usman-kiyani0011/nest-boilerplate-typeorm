93a9f5a8f88b4c5a9c486a0900efeb8aad1f7f62
Usman Kiyani <software04@herofun-bio.com>
2024-07-18 14:25:26 +0500

    added throttlermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 10000,
+        limit: 50, // 50 request in 10 seconds
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
