21e6ba9eba0755e2d3d33b8bd030225de8b09824
Usman Kiyani <software04@herofun-bio.com>
2024-07-30 14:52:25 +0500

    refactor(system user schema) : remove extra code

--- src/database/entities/system-user.entity.ts
+++ src/database/entities/system-user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, Index } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { UserRole } from '@shared/constants';
 
